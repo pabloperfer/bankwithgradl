c03eb50db24bc23e858ff39d197177a5c814de4f
Pablo Perez Fernandez <pablo.perez1981@gmail.com>
2022-04-15 12:17:13 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ tasks.named<JavaExec>("run") {
 }
 
 tasks.getByName<Test>("test") {
+    source = fileTree("/home/runner/work/bankwithgradl/bankwithgradl/public")
     doFirst {
         println(" executing the unit tests... ")
     }
@@ -52,12 +53,13 @@ checkstyle {
 
 tasks.register<Checkstyle>("checkstyle") {
     println("Checking coding style warnings and errors before unit tests.......")
-    source = fileTree("src/main/java")
+    source = fileTree("/home/runner/work/bankwithgradl/bankwithgradl/public")
     classpath = files()
     dependsOn ("spotbugs")
 }
 
 spotbugs {
+    source = fileTree("/home/runner/work/bankwithgradl/bankwithgradl/public")
     showProgress.set(true)
     tasks.spotbugsMain {
         dependsOn("test")
